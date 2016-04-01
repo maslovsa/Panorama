5ae00cfb953255829f2ae6121c280f3a00d458b8
Sergey Maslov <sergey.maslov@rokolabs.com>
2016-04-01 12:33:06 +0300

    init

---
+++ Podfile
@@ -0,0 +1,6 @@
+# source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+
+target 'Panorama' do
+pod 'CardboardSDK'
+end
---
+++ Pods/Target Support Files/Pods-Panorama/Pods-Panorama-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PanoramaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PanoramaVersionString[];
+
